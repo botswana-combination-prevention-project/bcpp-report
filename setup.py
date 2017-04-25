d5bfe805a941afd3292fc4b627c65ee4541cca9b
erikvw <ew2789@gmail.com>
2017-04-25 17:45:32 +0200

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='bcpp-report',
-    version='0.1.1',
+    version='0.1.2',
     author=u'Coulson Thabo Kgathi',
     author_email='ckgathi@gmail.com',
     packages=find_packages(),
